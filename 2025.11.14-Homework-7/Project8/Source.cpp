b3352c432d385671b7ad29facd526ce98cebd03d
Ilnur0406 <xyper0977@gmail.com>
2025-11-20 19:16:19 +0300

    2025.11.14-Homework-7>

---
+++ 2025.11.14-Homework-7/Project8/Source.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+long long Factorial (int n)
+{
+	long long f = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		f *= i;
+	}
+	return f;
+}
+int main(int argc, char** argv)
+{
+	int a = 0;
+	int b = 0;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	if ((a <= 20 || b <= 20))
+	{
+		printf("%lld", Factorial(a) / (Factorial(b) * Factorial(a - b)));
+	}
+	return 0;
+}
